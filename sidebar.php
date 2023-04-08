ce74550b5249246eff08c68727cf6f37b09708d9
nuriaulfah <114793248+nuriaulfah@users.noreply.github.com>
2023-04-08 21:47:04 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,39 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="index.html">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-menu-button-wide"></i><span>KAMPUS</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="view_inputdosen.php">
+          <i class="bi bi-circle"></i><span>Dosen</span>
+        </a>
+      </li>
+      <li>
+        <a href="view_inputmahasiswa.php">
+          <i class="bi bi-circle"></i><span>Mahasiswa</span>
+        </a>
+      </li>
+      <li>
+        <a href="view_inputstaff.php">
+          <i class="bi bi-circle"></i><span>Staff</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Components Nav -->
+
+
+</ul>
+
+</aside><!-- End Sidebar-->
